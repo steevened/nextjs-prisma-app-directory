48c1264cce7475bfd0c01f5add885d2978db132e
steevened <alvaradosteven315@gmail.com>
2023-09-24 13:17:49 -0500

    create api route to create stores

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,7 +1,30 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
 export async function POST(req: Request) {
-try {
-    
-} catch (error) {
-    console.log(error       )
-}
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const body = await req.json();
+    const { name } = body;
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    const store = prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORES] [POST] error", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
 }
